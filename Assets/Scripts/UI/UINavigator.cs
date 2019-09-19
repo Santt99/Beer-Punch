ce094c0f40b7a8c93190dbc0c8bca5307144fe49
Santiago Juarez Badillo Chavez <santiago10.jbch@gmail.com>
2019-09-19 08:36:00 -0500

    ADDED: Navigate in UI using Controller

---
+++ Assets/Scripts/UI/UINavigator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+public class UINavigator : MonoBehaviour
+{
+    private GameObject[] buttons;
+
+    private void Awake() {
+        buttons = GameObject.FindGameObjectsWithTag("Button");
+        Debug.Log(buttons.Length);
+    }
+    void Start()
+    {
+        buttons[0].GetComponent<Button>().Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
