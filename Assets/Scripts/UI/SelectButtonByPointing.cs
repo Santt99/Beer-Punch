a39c644d2ea4b045358d463524f93ffd6f855b50
Santiago Juarez Badillo Chavez <santiago10.jbch@gmail.com>
2019-09-19 20:14:41 -0500

    FIXED: Main manu navigation with controller, keyboard and mouse has been fixed

---
+++ Assets/Scripts/UI/SelectButtonByPointing.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;//
+public class SelectButtonByPointing : MonoBehaviour, IPointerEnterHandler
+{
+    public void OnPointerEnter (PointerEventData eventData) 
+	{
+		
+        this.GetComponent<Button>().Select();
+	}
+}
