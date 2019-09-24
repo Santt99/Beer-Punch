c350b1e8928f2471e47a75f1ec1b99ef6d3c1175
Santiago Juarez Badillo Chavez <santiago10.jbch@gmail.com>
2019-09-24 08:27:53 -0500

    ADDED: Settings Menu Scene, Return to menu button, graphics quality, sound volume, and music volume

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+public class MenuController : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
