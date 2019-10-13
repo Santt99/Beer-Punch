873e023adebde79e7c90c776ef7b6f2c0cccc202
Santiago Juarez Badillo Chavez <santiago10.jbch@gmail.com>
2019-10-13 11:34:11 -0500

    ADDED: Pause Menu

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public KeyCode keyToPause;
+    public Canvas pauseMenu;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPause))
+        {
+            activateOrDesactivatePauseMenu();
+        }
+    }
+
+    public void activateOrDesactivatePauseMenu()
+    {
+        pauseMenu.enabled = !pauseMenu.enabled;
+        if (pauseMenu.enabled)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+}
